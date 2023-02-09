57d410a7e7f1cd68cdb08bc7a162fafdf70c6dcf
Shadowelec1986 <38089172+Shadowelec1986@users.noreply.github.com>
2023-02-09 09:36:25 -0500

    scena first

---
+++ Assets/Scripts/DeteccionColision.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionColision : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject triguer;
+    public GameObject _interactiveObject;
+    public GameObject jugador;
+
+
+    private bool activated = false;
+
+
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider jugador)
+    {
+        if (jugador != null)
+        {
+            Debug.Log("Entro");
+            if (!activated)
+            {
+
+                _interactiveObject.SetActive(true);
+                
+            }
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider jugador)
+    {
+
+        if (jugador != null)
+        {
+            Debug.Log("Salio");
+            if (!activated)
+            {
+                _interactiveObject.SetActive(false);
+                
+            }
+
+
+        }
+
+
+    }
+}
+
+//if (other.tag == "Jugador")
